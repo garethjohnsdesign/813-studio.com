cbf189059c266894ac7ed08f67a86e63cb08c63c
Gareth Johns <info@garethjohnsdesign.com>
2017-02-22 20:03:43 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# 813 Studio
+
+Website for 813 Studio. Please go to [813-studio.com](http://813-studio.com) in case you want to take a look at the website.
+
+* * *
+
+Copyright (c) 2017 Gareth Johns Art Direction + Design – info@garethjohnsdesign.com
---
+++ _site/assets/js/app.js
@@ -0,0 +1,36 @@
+$(document).foundation();
+
+
+/*
+	var colours = [
+		'#548AEA',
+		'#FF8F7B',
+		'#CAE3FC',
+		'#315E47',
+		'#F8B494',
+		'#B5D536',
+		'#EDDA46',
+		'#94877E',
+		'#FBDBEB',
+		'#912F65'
+	];
+
+	var colourIndex = 0;
+
+	
+		$('a').mouseenter(function(){
+			colourIndex = colourIndex === 9 ? 0 : colourIndex + 1;
+			$('body').css({background:colours[colourIndex]}).addClass('dark');
+		}).mouseleave(function(){
+			$('body').removeClass('dark').css({background:'#fff'});
+		}).click(function(){
+			$(this).addClass('strike');
+		});
+*/
+	
+		
+$('a').mouseenter(function(){
+	$('body').addClass('dark');
+}).mouseleave(function(){
+	$('body').removeClass('dark');
+});
